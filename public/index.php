780eea35b20d57bfe5cb107b40694240812ab530
David Maus <dmaus@dmaus.name>
2021-09-08 10:35:53 +0200

    Simple front controller

---
+++ public/index.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../config/service.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+switch ($request->getMethod()) {
+case 'GET':
+case 'HEAD':
+    $parameters = new HAB\OAI\PMH\Request\Parameters($request->query->all());
+    break;
+case 'POST':
+    $parameters = new HAB\OAI\PMH\Request\Parameters($request->request->all());
+    break;
+default:
+    $response = new Response(Response::$statusTexts[405], 405, ['Allow' => 'GET, HEAD, POST']);
+    $response->prepare($request);
+    $response->send();
+    exit;
+}
+
+$repository = new SubHH\Linkshare\OAI\Repository($container);
+$controller = new HAB\OAI\PMH\Request\Controller($repository);
+
+try {
+    $payload = $controller->handle('https://linkshare.sub.uni-hamburg.de/service/oai', $parameters);
+
+    $writer = new HAB\OAI\PMH\Response\Writer(true);
+    $body = $writer->serialize($payload);
+
+    $response = new Response($body, 200, ['Content-Type' => 'text/xml; charset=utf-8']);
+    $response->prepare($request);
+    $response->send();
+    exit;
+} catch (Throwable $e) {
+    $response = new Response(Response::$statusTexts[500], 500);
+    $response->prepare($request);
+    $response->send();
+    exit;
+}
